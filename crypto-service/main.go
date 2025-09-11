2917a7e8f8a2225d9361d780b9c19eaeda3c2f04
TasiusKenways <88521859+tasiuskenways@users.noreply.github.com>
2025-09-11 20:39:23 +0700

    Feat: Add Flagging Command To Generate Keys In crypto-service

--- crypto-service/main.go
+++ crypto-service/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
@@ -9,9 +10,26 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/requestid"
 	"tasius.my.id/SE/crypto-service/internal/config"
 	"tasius.my.id/SE/crypto-service/internal/interfaces/http/routes"
+	"tasius.my.id/SE/crypto-service/internal/utils/crypto"
 )
 
-func main()  {
+func main() {
+
+	generateKeys := flag.Bool("generateKeys", false, "Generate new keys")
+	flag.Parse()
+
+	if *generateKeys {
+		filePath := flag.String("filePath", "./keys", "Path to the keys directory")
+		flag.Parse()
+
+		privateKeyPath, publicKeyPath, err := crypto.GenerateAndSaveRSAKeyPair(*filePath)
+		if err != nil {
+			log.Fatal("Failed to generate and save RSA key pair:", err)
+		}
+		log.Println("Private key saved to:", privateKeyPath)
+		log.Println("Public key saved to:", publicKeyPath)
+		return
+	}
 
 	cfg := config.Load()
 
@@ -38,10 +56,10 @@ func main()  {
 	}))
 
 	routes.SetupRoutes(app, cfg)
-	
+
 	log.Printf("Server starting on port %s", cfg.AppPort)
 	if err := app.Listen(":" + cfg.AppPort); err != nil {
 		log.Fatal("Failed to start server:", err)
 	}
-	
+
 }
